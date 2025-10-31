71dc38f2ad4868f046cff84bb05d5c97fffa9034
thybui1903 <thybui1903qn@gmail.com>
2025-10-31 23:15:51 +0700

    modified:   LAB_2/model/GoogleNet.py

--- LAB_2/model/GoogleNet.py
+++ LAB_2/model/GoogleNet.py
@@ -42,7 +42,7 @@ class GoogleNet(nn.Module):
     def __init__(self):
         super().__init__()
         
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3)
         self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
         
         self.conv2_1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1, stride=1, padding=0)
